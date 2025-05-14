bccac51bc99cf746407996d64703edc5cef11943
siddhiagarkar <siddhiagarkar1203@gmail.com>
2025-05-14 19:44:15 +0530

    created smart contract and local deployment

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+// STEP 2
+
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.28",
+
+  networks: {
+    hardhat: {
+      chainId: 1337, // Local blockchain ID
+    },
+  },
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+// STEP 3
+
+const hre = require("hardhat");
+
+async function main() {
+  const CertificateVerification = await hre.ethers.getContractFactory("CertificateVerification");
+  const contract = await CertificateVerification.deploy();
+
+  await contract.waitForDeployment();
+
+  console.log("Contract deployed to:", await contract.getAddress());
+  //the smart contract gets deployed on the blockchain network. and you get an address using which 
+  //you can use the functions in the contract
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
